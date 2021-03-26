ef72a58f629d1cf8c00ea532178f993c08043d5f
Phillip Maier <pmaier983@gmail.com>
2021-03-25 22:10:17 -0400

    create basic api

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import express from "express"
+import { graphqlHTTP } from "express-graphql"
+import { buildSchema } from "graphql"
+
+// Create a server:
+const app = express()
+
+// Create a schema and a root resolver:
+const schema = buildSchema(`
+    type Book {
+        title: String!
+        author: String!
+    }
+
+    type Query {
+        books: [Book]
+    }
+`)
+
+const rootValue = {
+  books: [
+    {
+      title: "The Name of the Wind",
+      author: "Patrick Rothfuss",
+    },
+    {
+      title: "The Wise Man's Fear",
+      author: "Patrick Rothfuss",
+    },
+  ],
+}
+
+// Use those to handle incoming requests:
+app.use(
+  graphqlHTTP({
+    schema,
+    rootValue,
+  })
+)
+
+// Start the server:
+app.listen(8080, () => console.log("Server started on port 8080"))
