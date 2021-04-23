52fa97d1330223a3a6a6233735e08ba8ad7e6e8c
Phillip Maier <pmaier983@gmail.com>
2021-04-23 14:20:18 -0400

    Setup OAuth as a task & use pg-promise fetchers

--- src/googleOAuth.ts
+++ src/googleOAuth.ts
@@ -1,6 +1,5 @@
 import { Strategy as GoogleStrategy } from "passport-google-oauth2"
 import argon2 from "argon2"
-
 import db from "./database"
 
 const {
@@ -16,72 +15,70 @@ export const googleOAuthStrategy = new GoogleStrategy(
     callbackURL: "http://localhost:8080/google",
     passReqToCallback: true,
   },
-  // TODO: use a task
-  async (request, accessToken, refreshToken, profile, done) => {
-    // STEP 1 Check that the user is verified. If not reject.
-    if (!profile.email_verified || !profile.verified) {
-      // prevent a unverified user from creating an account to avoid
-      // account theft via fake email creation (once I have multiple Auths setup)
-      return done(Error("You must verify your Account to log in"))
-    }
-
-    const userEmail = profile.email
-
-    const isUserInDb = await db.query(
-      "SELECT 1 FROM public.user WHERE email = $1",
-      userEmail
-    )
-
-    // TODO: Possible better pepper strategy out there (symmetrical encryption)
-    // https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#peppering
-    // TODO stop using email as password... find a better way to do this.
-    const userRawPassword = PASSWORD_PEPPER + profile.email
-
-    // STEP 2: if there is not user in the DB create one.
-    if (!isUserInDb.length) {
-      const newUserInfo = {
-        email: userEmail,
-        password: await argon2.hash(userRawPassword),
-        time_created: Date.now(),
-        username: "NamelessSailor",
-        username_update_time: 0, // default date is start of epoch
+  // TODO: are both async needed here?
+  (req, accessToken, refreshToken, profile, done) =>
+    db.task(async (t) => {
+      // STEP 1 Check that the user is verified. If not reject.
+      if (!profile.email_verified || !profile.verified) {
+        // prevent a unverified user from creating an account to avoid
+        // account theft via fake email creation (once I have multiple Auths setup)
+        return done(Error("You must verify your Account to log in"))
       }
-      // insert them into the database
-      db.query(
-        "INSERT into public.user (password, time_created, email, username, username_update_time) VALUES (${password}, ${time_created}, ${email}, ${username}, ${username_update_time})",
-        newUserInfo
+
+      const userEmail = profile.email
+
+      const countOfUsersInDb = await t.oneOrNone(
+        "SELECT count(*) FROM public.user WHERE email = $1",
+        userEmail,
+        ({ count }) => parseInt(count, 10)
       )
-    }
 
-    // STEP 3: Fetch your user (possibly newly created) and verify their password
-    const hashedPassword = await db.query(
-      "SELECT password FROM public.user WHERE email = $1",
-      userEmail
-    )
+      // If there are multiple users with the same email in the db something went wrong
+      if (countOfUsersInDb > 1) {
+        return done(Error("Something went wrong, Please try again"))
+      }
 
-    const isPasswordVerified = await argon2.verify(
-      hashedPassword?.[0]?.password,
-      userRawPassword
-    )
+      // TODO: Possible better pepper strategy out there (symmetrical encryption)
+      // https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#peppering
+      // TODO stop using email as password... find a better way to do this.
+      const userRawPassword = PASSWORD_PEPPER + profile.email
 
-    if (!isPasswordVerified) {
-      return done(Error("Your username and password did not match"))
-    }
+      // STEP 2: if there is not user in the DB create one.
+      if (countOfUsersInDb === 0) {
+        const newUserInfo = {
+          email: userEmail,
+          password: await argon2.hash(userRawPassword),
+          time_created: Date.now(),
+          username: "NamelessSailor",
+          username_update_time: 0, // default date is start of epoch
+        }
+        // insert them into the database
+        t.none(
+          "INSERT into public.user (password, time_created, email, username, username_update_time) VALUES (${password}, ${time_created}, ${email}, ${username}, ${username_update_time})",
+          newUserInfo
+        )
+      }
 
-    const user = await db.query(
-      "SELECT email, time_created, username, uuid, username_update_time FROM public.user WHERE email = $1",
-      userEmail
-    )
+      // STEP 3: Fetch your user (possibly newly created) and verify their password
+      const fullUser = await t.one(
+        "SELECT * FROM public.user WHERE email = $1",
+        userEmail
+      )
 
-    // This should never happen...
-    if (!user.length || user.length >= 2) {
-      // TODO add logging here
-      return done(Error("Something went wrong, Please try again"))
-    }
+      // This should never happen...
+      if (!fullUser) {
+        // TODO add logging here
+        return done(Error("Something went wrong, Please try again"))
+      }
 
-    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //   return done(err, user)
-    // })
-    done(null, user[0])
-  }
+      const { password, ...userLessPassword } = fullUser
+
+      const isPasswordVerified = await argon2.verify(password, userRawPassword)
+
+      if (!isPasswordVerified) {
+        return done(Error("Your username and password did not match"))
+      }
+
+      done(null, userLessPassword)
+    })
 )
