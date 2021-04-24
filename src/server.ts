7475c0a3fd44b2fb42b6a32193668fe52c7d91ff
Phillip Maier <pmaier983@gmail.com>
2021-04-24 19:24:22 -0400

    Setup Server to respond with jwt at /google

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import passport from "passport"
 import { graphqlHTTP } from "express-graphql"
+import jwt from "jsonwebtoken"
 
 import db from "@/database"
 import { schema } from "./schema"
@@ -9,6 +10,9 @@ import { googleOAuthStrategy } from "./googleOAuth"
 // TODO: GZIP response
 // TODO: setup a dataLoader
 // TODO: setup some database mocking (msw, json-server, etc?)
+// TODO: setup some bundle size reporter?
+
+const { JWT_SECRET } = process.env
 
 // Create a server:
 const app = express()
@@ -16,23 +20,26 @@ const app = express()
 app.use(passport.initialize())
 
 passport.use(googleOAuthStrategy)
+
 // TODO: change all of these to GRAPHQL (/graphql) url if possible!
-app.get("/google/success", (req, res) => {
-  // TODO: send back to home page with JWT?
-  return res.send(`You're in Phillip`)
-})
-
-// TODO: send back with error
-app.get("/google/failure", (req, res) => res.send(`No Touch!`))
-
 app.get(
   "/google",
   passport.authenticate("google", {
-    scope: ["email", "profile"],
-    successRedirect: "/google/success",
-    failureRedirect: "/google/failure",
+    scope: ["profile", "email"],
     session: false,
-  })
+    passReqToCallback: true,
+  }),
+  (req, res) => {
+    // TODO: how to properly handle Errors next(error?)
+    if (!req?.user) {
+      throw Error("Something went very wrong")
+    }
+    // TODO best patter for api responses?
+    res.send({
+      user: req?.user,
+      token: jwt.sign(req.user, JWT_SECRET, { expiresIn: "30 days" }),
+    })
+  }
 )
 
 // Use those to handle incoming requests:
@@ -40,6 +47,7 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema,
+    // TODO setup passport-jwt auth.
     context: {
       db,
     },
