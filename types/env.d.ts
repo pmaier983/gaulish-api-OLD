39325e26afa10bcbe74e2ebc9a47fea9179667e9
Phillip Maier <pmaier983@gmail.com>
2021-05-06 19:51:40 -0400

    Setup proper .env typings

---
+++ types/env.d.ts
@@ -0,0 +1,28 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production"
+      CLOUDFRONT_DISTRIBUTION_ID: string
+      S3_BUCKET_NAME: string
+      AWS_ACCOUNT_NUMBER: string
+      AWS_REGION: string
+      RDS_USER: string
+      RDS_HOST: string
+      RDS_DATABASE: string
+      RDS_PASSWORD: string
+      RDS_PORT: string
+      GOOGLE_OAUTH_CLIENT_ID: string
+      GOOGLE_OAUTH_CLIENT_SECRET: string
+      PASSWORD_PEPPER: string
+      JWT_SECRET: string
+      DOCKER_WORKDIR: string
+      GRAPHQL_PORT: string
+      FRONTEND_DEV_URL: string
+      FRONTEND_URL: string
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {}
