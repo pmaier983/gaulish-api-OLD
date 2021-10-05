5e168b6748676544d70bd6bb748a56350f2d8493
Phillip ED Maier <pmaier983@gmail.com>
2021-10-04 21:29:59 -0700

    Upload query for test map

---
+++ QUERIES.sql
@@ -0,0 +1,59 @@
+insert into 
+	public.tile(x, y, type) 
+values 
+	(0, 6, 'ocean'),
+  (0, 5, 'ocean'),
+  (0, 4, 'ocean'),
+  (0, 3, 'ocean'),
+  (0, 2, 'meadows'),
+  (0, 1, 'forest'),
+  (0, 0, 'mountains'),
+
+  (1, 6, 'ocean'),
+  (1, 5, 'meadows'),
+  (1, 4, 'ocean'),
+  (1, 3, 'meadows'),
+  (1, 2, 'meadows'),
+  (1, 1, 'forest'),
+  (1, 0, 'forest'),
+
+  (2, 6, 'ocean'),
+  (2, 5, 'ocean'),
+  (2, 4, 'ocean'),
+  (2, 3, 'ocean'),
+  (2, 2, 'meadows'),
+  (2, 1, 'meadows'),
+  (2, 0, 'meadows'),
+
+  (3, 6, 'ocean'),
+  (3, 5, 'ocean'),
+  (3, 4, 'mountains'),
+  (3, 3, 'ocean'),
+  (3, 2, 'ocean'),
+  (3, 1, 'ocean'),
+  (3, 0, 'ocean'),
+
+  (4, 6, 'ocean'),
+  (4, 5, 'ocean'),
+  (4, 4, 'ocean'),
+  (4, 3, 'mountains'),
+  (4, 2, 'mountains'),
+  (4, 1, 'ocean'),
+  (4, 0, 'ocean'),
+
+  (5, 6, 'ocean'),
+  (5, 5, 'meadows'),
+  (5, 4, 'ocean'),
+  (5, 3, 'ocean'),
+  (5, 2, 'ocean'),
+  (5, 1, 'ocean'),
+  (5, 0, 'ocean'),
+
+  (6, 6, 'ocean'),
+  (6, 5, 'forest'),
+  (6, 4, 'meadows'),
+  (6, 3, 'ocean'),
+  (6, 2, 'ocean'),
+  (6, 1, 'ocean'),
+  (6, 0, 'meadows')
+returning *;
