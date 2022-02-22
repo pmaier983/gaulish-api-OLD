e2916e122db04eeb57d03cc507c1357ab59323d9
Phillip ED Maier <pmaier983@gmail.com>
2022-02-21 18:20:28 -0800

    Add response builder & nesting to city

--- src/api/city.ts
+++ src/api/city.ts
@@ -1,6 +1,6 @@
 import gql from "graphql-tag"
 
-import { Resolvers, City } from "@/graphql-types"
+import { Resolvers, City, Tile } from "@/graphql-types"
 import { addGlobalID } from "@/utils"
 
 export const typeDefs = gql`
@@ -13,20 +13,39 @@ export const typeDefs = gql`
     city_id: Int!
     name: String!
     tile_id: Int! # any way to have this be Tile.id?
-    x: Int!
-    y: Int!
-    type: TileTypes!
+    # would it be better to nest this under tile: Tile?
+    tile: Tile!
   }
 `
+
+type CityFromDB = Omit<City, "id"> & Omit<Tile, "id">
+
+export const buildCitiesResponse = (noIdCities: CityFromDB[]): City[] => {
+  const cities = noIdCities.map((cur) => {
+    const tile = {
+      tile_id: cur.tile_id,
+      x: cur.x,
+      y: cur.y,
+      type: cur.type,
+    }
+    return {
+      city_id: cur.city_id,
+      name: cur.name,
+      tile: addGlobalID("tile", "tile_id", tile),
+    }
+  })
+  return addGlobalID("city", "city_id", cities)
+}
 
 export const resolvers: Resolvers = {
   Query: {
     getAllCities: async (_obj, _args, context) => {
-      const cities: City[] = await context.db
+      const flatCities: CityFromDB[] = await context.db
         .any(`select * from public.city Cities
               join public.tile Tiles
               on Cities.tile_id = Tiles.tile_id`)
-      return addGlobalID<City[]>("city", "city_id", cities)
+      // TODO: is there a better way of doing this?
+      return buildCitiesResponse(flatCities)
     },
   },
 }
