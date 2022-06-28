40fa461d83c360bffb41281faf8cb607e4a687f7
Phillip ED Maier <pmaier983@gmail.com>
2022-06-27 21:03:40 -0700

    add user to context

--- src/context.ts
+++ src/context.ts
@@ -1,13 +1,16 @@
 import db from "@/database"
 import { Database } from "database"
 import { DataLoaders, dataLoaders } from "./dataLoaders"
+import { User } from "@/generated/graphql"
 
 export interface Context {
   db: Database
   dataLoaders: DataLoaders
+  user: User
 }
 
 export const context: Context = {
   db,
   dataLoaders,
+  user: { id: "Shit Something went wrong" }, // TODO: how to initialize this?
 }
