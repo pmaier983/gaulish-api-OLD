96e1edae842e7139f4fe50fd751d77314fcb4c00
Phillip ED Maier <pmaier983@gmail.com>
2022-06-28 20:59:31 -0700

    path creation using postgres

--- src/api/path.ts
+++ src/api/path.ts
@@ -23,14 +23,41 @@ export const resolvers: Resolvers = {
     setShipPath: async (_obj, { ship_id, shipPath }, context) => {
       // TODO: duplicate validation here!
       // TODO why not just use template literals
-      context.db.one(
-        "insert into public.path (ship_id, start_time, path) values (${ship_id}, ${start_time}, ${shipPath})",
-        {
-          ship_id,
-          start_time: Date.now(),
-          shipPath,
-        }
-      )
+
+      const shipPathArray = shipPathArrayFromString(shipPath)
+
+      context.db.task(async (taskDb) => {
+        taskDb.one(
+          "insert into public.path (ship_id, start_time, path) values (${ship_id}, ${start_time}, ${shipPath})",
+          {
+            ship_id,
+            start_time: Date.now(),
+            shipPath,
+          }
+        )
+
+        const ship_type_id = await taskDb.one(
+          `select ship_type_id from public.ship where ship_id = $1`,
+          ship_id
+        )
+        const shipSpeed = shipTypes[ship_type_id].speed
+
+        const journeyLength = shipPathArray.length
+        const timeToDestination = journeyLength * shipSpeed
+
+        const destinationCityId = await taskDb.one(
+          `select city_id from public.city where tile_id = $1`,
+          shipPathArray.at(-1).tile_id
+        )
+
+        setTimeout(() => {
+          taskDb.one(
+            "update public.ship set city_id = ${destinationCityId} where ship_id = ${ship_id}",
+            { destinationCityId, ship_id }
+          )
+        }, timeToDestination)
+      })
+
       return false
     },
   },
