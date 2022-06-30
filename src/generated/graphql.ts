b3d01f3627f4681c01eca80efcd80f69da57529f
Phillip ED Maier <pmaier983@gmail.com>
2022-06-30 07:31:11 -0700

    idk what i did but i fixed things

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -1,9 +1,8 @@
+/* 🌶️ This is a generated file, do not modify! */
 import { GraphQLResolveInfo } from "graphql"
 import { Context } from "../context"
-export type RequireFields<T, K extends keyof T> = {
-  [X in Exclude<keyof T, K>]?: T[X]
-} & { [P in K]-?: NonNullable<T[P]> }
 export type Maybe<T> = T | null
+export type InputMaybe<T> = Maybe<T>
 export type Exact<T extends { [key: string]: unknown }> = {
   [K in keyof T]: T[K]
 }
@@ -12,6 +11,181 @@ export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
 }
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
   [SubKey in K]: Maybe<T[SubKey]>
+}
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]-?: NonNullable<T[P]>
+}
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string
+  String: string
+  Boolean: boolean
+  Int: number
+  Float: number
+}
+
+export type Chat = Node & {
+  __typename?: "Chat"
+  chat_id?: Maybe<Scalars["Int"]>
+  id: Scalars["ID"]
+  recipient_uuid?: Maybe<Scalars["Int"]>
+  room_id?: Maybe<Scalars["Int"]>
+  text?: Maybe<Scalars["String"]>
+  timestamp?: Maybe<Scalars["Int"]>
+  uuid?: Maybe<Scalars["Int"]>
+}
+
+export type City = Node & {
+  __typename?: "City"
+  city_id: Scalars["Int"]
+  id: Scalars["ID"]
+  name: Scalars["String"]
+  tile: Tile
+}
+
+export type Log = Node & {
+  __typename?: "Log"
+  id: Scalars["ID"]
+  log_id: Scalars["Int"]
+  text?: Maybe<Scalars["String"]>
+  type: Scalars["Int"]
+  uuid: Scalars["Int"]
+}
+
+export type Mutation = {
+  __typename?: "Mutation"
+  setLog: Scalars["Int"]
+  setShipPath?: Maybe<Scalars["Boolean"]>
+}
+
+export type MutationSetLogArgs = {
+  text: Scalars["String"]
+  type: Scalars["Int"]
+}
+
+export type MutationSetShipPathArgs = {
+  shipPath: Scalars["String"]
+  ship_id: Scalars["Int"]
+}
+
+export type Node = {
+  id: Scalars["ID"]
+}
+
+export type Npc = Node & {
+  __typename?: "Npc"
+  id: Scalars["ID"]
+  path: Array<Tile>
+  ship_type: ShipType
+  should_repeat: Scalars["Boolean"]
+  start_time: Scalars["Int"]
+}
+
+export type Path = Node & {
+  __typename?: "Path"
+  id: Scalars["ID"]
+  path: Scalars["String"]
+  path_id: Scalars["Int"]
+  ship_id: Scalars["Int"]
+  start_time: Scalars["Int"]
+}
+
+export type Point = {
+  x?: InputMaybe<Scalars["Int"]>
+  y?: InputMaybe<Scalars["Int"]>
+}
+
+export type Query = {
+  __typename?: "Query"
+  getAllCities: Array<City>
+  getAllNpcs: Array<Npc>
+  getAllTiles: Array<Tile>
+  getChatHistory: Array<Chat>
+  getRecentLogs?: Maybe<Array<Maybe<Log>>>
+  getShipTypeFromId: ShipType
+  getShipsByUUID: Array<Ship>
+  getTileByID: Tile
+  getTilesAroundTile: Array<Tile>
+  getTilesWithinRectangle: Array<Tile>
+  getUserByUsername: User
+  verifyToken: Scalars["Boolean"]
+}
+
+export type QueryGetChatHistoryArgs = {
+  room_id?: InputMaybe<Scalars["Int"]>
+  timestamp: Scalars["Int"]
+}
+
+export type QueryGetShipTypeFromIdArgs = {
+  shipTypeId?: InputMaybe<Scalars["Int"]>
+}
+
+export type QueryGetShipsByUuidArgs = {
+  uuid?: InputMaybe<Scalars["Int"]>
+}
+
+export type QueryGetTileByIdArgs = {
+  tileId?: InputMaybe<Scalars["Int"]>
+}
+
+export type QueryGetTilesAroundTileArgs = {
+  point?: InputMaybe<Point>
+  radius?: InputMaybe<Scalars["Int"]>
+}
+
+export type QueryGetTilesWithinRectangleArgs = {
+  pointA?: InputMaybe<Point>
+  pointB?: InputMaybe<Point>
+}
+
+export type QueryGetUserByUsernameArgs = {
+  username?: InputMaybe<Scalars["String"]>
+}
+
+export type Ship = Node & {
+  __typename?: "Ship"
+  city: City
+  id: Scalars["ID"]
+  name: Scalars["String"]
+  ship_id: Scalars["Int"]
+  ship_type: ShipType
+  uuid: Scalars["Int"]
+}
+
+export type ShipType = Node & {
+  __typename?: "ShipType"
+  attack_rating: Scalars["Int"]
+  cargo_capacity: Scalars["Int"]
+  id: Scalars["ID"]
+  inventory_slots: Scalars["Int"]
+  name: Scalars["String"]
+  ship_type_id: Scalars["Int"]
+  speed: Scalars["Int"]
+}
+
+export type Tile = Node & {
+  __typename?: "Tile"
+  id: Scalars["ID"]
+  tile_id: Scalars["Int"]
+  type: TileTypes
+  x: Scalars["Int"]
+  y: Scalars["Int"]
+}
+
+export enum TileTypes {
+  Forest = "forest",
+  Meadows = "meadows",
+  Mountains = "mountains",
+  Ocean = "ocean",
+}
+
+export type User = Node & {
+  __typename?: "User"
+  email?: Maybe<Scalars["String"]>
+  id: Scalars["ID"]
+  time_created?: Maybe<Scalars["String"]>
+  username?: Maybe<Scalars["String"]>
+  uuid?: Maybe<Scalars["Int"]>
 }
 
 export type ResolverTypeWrapper<T> = Promise<T> | T
@@ -35,7 +209,7 @@ export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
   args: TArgs,
   context: TContext,
   info: GraphQLResolveInfo
-) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
@@ -309,37 +483,37 @@ export type QueryResolvers<
     ResolversTypes["ShipType"],
     ParentType,
     ContextType,
-    RequireFields<QueryGetShipTypeFromIdArgs, never>
+    Partial<QueryGetShipTypeFromIdArgs>
   >
   getShipsByUUID?: Resolver<
     Array<ResolversTypes["Ship"]>,
     ParentType,
     ContextType,
-    RequireFields<QueryGetShipsByUuidArgs, never>
+    Partial<QueryGetShipsByUuidArgs>
   >
   getTileByID?: Resolver<
     ResolversTypes["Tile"],
     ParentType,
     ContextType,
-    RequireFields<QueryGetTileByIdArgs, never>
+    Partial<QueryGetTileByIdArgs>
   >
   getTilesAroundTile?: Resolver<
     Array<ResolversTypes["Tile"]>,
     ParentType,
     ContextType,
-    RequireFields<QueryGetTilesAroundTileArgs, never>
+    Partial<QueryGetTilesAroundTileArgs>
   >
   getTilesWithinRectangle?: Resolver<
     Array<ResolversTypes["Tile"]>,
     ParentType,
     ContextType,
-    RequireFields<QueryGetTilesWithinRectangleArgs, never>
+    Partial<QueryGetTilesWithinRectangleArgs>
   >
   getUserByUsername?: Resolver<
     ResolversTypes["User"],
     ParentType,
     ContextType,
-    RequireFields<QueryGetUserByUsernameArgs, never>
+    Partial<QueryGetUserByUsernameArgs>
   >
   verifyToken?: Resolver<ResolversTypes["Boolean"], ParentType, ContextType>
 }
@@ -412,177 +586,4 @@ export type Resolvers<ContextType = Context> = {
   ShipType?: ShipTypeResolvers<ContextType>
   Tile?: TileResolvers<ContextType>
   User?: UserResolvers<ContextType>
-}
-
-/** All built-in and custom scalars, mapped to their actual values */
-export type Scalars = {
-  ID: string
-  String: string
-  Boolean: boolean
-  Int: number
-  Float: number
-}
-
-export type Chat = Node & {
-  __typename?: "Chat"
-  chat_id?: Maybe<Scalars["Int"]>
-  id: Scalars["ID"]
-  recipient_uuid?: Maybe<Scalars["Int"]>
-  room_id?: Maybe<Scalars["Int"]>
-  text?: Maybe<Scalars["String"]>
-  timestamp?: Maybe<Scalars["Int"]>
-  uuid?: Maybe<Scalars["Int"]>
-}
-
-export type City = Node & {
-  __typename?: "City"
-  city_id: Scalars["Int"]
-  id: Scalars["ID"]
-  name: Scalars["String"]
-  tile: Tile
-}
-
-export type Log = Node & {
-  __typename?: "Log"
-  id: Scalars["ID"]
-  log_id: Scalars["Int"]
-  text?: Maybe<Scalars["String"]>
-  type: Scalars["Int"]
-  uuid: Scalars["Int"]
-}
-
-export type Mutation = {
-  __typename?: "Mutation"
-  setLog: Scalars["Int"]
-  setShipPath?: Maybe<Scalars["Boolean"]>
-}
-
-export type MutationSetLogArgs = {
-  text: Scalars["String"]
-  type: Scalars["Int"]
-}
-
-export type MutationSetShipPathArgs = {
-  shipPath: Scalars["String"]
-  ship_id: Scalars["Int"]
-}
-
-export type Node = {
-  id: Scalars["ID"]
-}
-
-export type Npc = Node & {
-  __typename?: "Npc"
-  id: Scalars["ID"]
-  path: Array<Tile>
-  ship_type: ShipType
-  should_repeat: Scalars["Boolean"]
-  start_time: Scalars["Int"]
-}
-
-export type Path = Node & {
-  __typename?: "Path"
-  id: Scalars["ID"]
-  path: Scalars["String"]
-  path_id: Scalars["Int"]
-  ship_id: Scalars["Int"]
-  start_time: Scalars["Int"]
-}
-
-export type Point = {
-  x?: Maybe<Scalars["Int"]>
-  y?: Maybe<Scalars["Int"]>
-}
-
-export type Query = {
-  __typename?: "Query"
-  getAllCities: Array<City>
-  getAllNpcs: Array<Npc>
-  getAllTiles: Array<Tile>
-  getChatHistory: Array<Chat>
-  getRecentLogs?: Maybe<Array<Maybe<Log>>>
-  getShipTypeFromId: ShipType
-  getShipsByUUID: Array<Ship>
-  getTileByID: Tile
-  getTilesAroundTile: Array<Tile>
-  getTilesWithinRectangle: Array<Tile>
-  getUserByUsername: User
-  verifyToken: Scalars["Boolean"]
-}
-
-export type QueryGetChatHistoryArgs = {
-  room_id?: Maybe<Scalars["Int"]>
-  timestamp: Scalars["Int"]
-}
-
-export type QueryGetShipTypeFromIdArgs = {
-  shipTypeId?: Maybe<Scalars["Int"]>
-}
-
-export type QueryGetShipsByUuidArgs = {
-  uuid?: Maybe<Scalars["Int"]>
-}
-
-export type QueryGetTileByIdArgs = {
-  tileId?: Maybe<Scalars["Int"]>
-}
-
-export type QueryGetTilesAroundTileArgs = {
-  point?: Maybe<Point>
-  radius?: Maybe<Scalars["Int"]>
-}
-
-export type QueryGetTilesWithinRectangleArgs = {
-  pointA?: Maybe<Point>
-  pointB?: Maybe<Point>
-}
-
-export type QueryGetUserByUsernameArgs = {
-  username?: Maybe<Scalars["String"]>
-}
-
-export type Ship = Node & {
-  __typename?: "Ship"
-  city: City
-  id: Scalars["ID"]
-  name: Scalars["String"]
-  ship_id: Scalars["Int"]
-  ship_type: ShipType
-  uuid: Scalars["Int"]
-}
-
-export type ShipType = Node & {
-  __typename?: "ShipType"
-  attack_rating: Scalars["Int"]
-  cargo_capacity: Scalars["Int"]
-  id: Scalars["ID"]
-  inventory_slots: Scalars["Int"]
-  name: Scalars["String"]
-  ship_type_id: Scalars["Int"]
-  speed: Scalars["Int"]
-}
-
-export type Tile = Node & {
-  __typename?: "Tile"
-  id: Scalars["ID"]
-  tile_id: Scalars["Int"]
-  type: TileTypes
-  x: Scalars["Int"]
-  y: Scalars["Int"]
-}
-
-export enum TileTypes {
-  Forest = "forest",
-  Meadows = "meadows",
-  Mountains = "mountains",
-  Ocean = "ocean",
-}
-
-export type User = Node & {
-  __typename?: "User"
-  email?: Maybe<Scalars["String"]>
-  id: Scalars["ID"]
-  time_created?: Maybe<Scalars["String"]>
-  username?: Maybe<Scalars["String"]>
-  uuid?: Maybe<Scalars["Int"]>
 }
