d146a756967ee98e9c7af82b7eae5ffc5ecd61a9
Phillip ED Maier <pmaier983@gmail.com>
2022-07-07 07:13:10 -0700

    Add timestamp to log and overload

--- src/api/log.ts
+++ src/api/log.ts
@@ -15,39 +15,56 @@ export const typeDefs = gql`
   }
 
   extend type Mutation {
-    setLog(type: Int!, text: String!): Int! # TODO: return int or boolean?
+    setLog(type: Int!, text: String!, timestamp: Int): Int! # TODO: return int or boolean?
   }
 
   type Log implements Node {
     id: ID!
     log_id: Int!
     uuid: Int!
-    text: String
+    text: String!
     type: Int!
+    timestamp: Int!
   }
 `
 
-export const setLog = async ({
+type SetLog = {
+  (params: {
+    type: LogTypes
+    text: string
+    timestamp?: number
+    context: Context
+  }): Promise<number>
+  (params: {
+    type: LogTypes
+    text: string
+    timestamp?: number
+    taskDb: Database
+  }): Promise<number>
+}
+
+export const setLog: SetLog = async ({
   type,
   text,
+  timestamp,
   context,
-}: {
-  type: typeof LogTypes
-  text: string
-  context: Context
-}): Promise<number> =>
-  context.db.one(
+  taskDb,
+}) => {
+  const db = taskDb ? taskDb : context.db
+  return db.one(
     `insert into public.log 
-  (text, type, uuid) values 
-  ($(text), $(type), $(uuid)) 
+  (text, type, uuid, timestamp) values 
+  ($(text), $(type), $(uuid), $(timestamp)) 
   returning log_id`,
     {
       uuid: 1,
       type,
       text,
+      timestamp: Number.isNaN(timestamp) ? Date.now() : timestamp,
     },
     (res) => parseInt(res.log_id, 10)
   )
+}
 
 export const resolvers: Resolvers = {
   Query: {
@@ -62,10 +79,10 @@ export const resolvers: Resolvers = {
     },
   },
   Mutation: {
-    setLog: async (obj, { type, text }, context) => {
+    setLog: async (obj, { type, text, timestamp }, context) => {
       // TODO: affirm type is LogTypes
       const theType: any = type
-      return await setLog({ type: theType, text, context })
+      return await setLog({ type: theType, text, timestamp, context })
     },
   },
 }
